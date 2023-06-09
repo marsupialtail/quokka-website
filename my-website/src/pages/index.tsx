43fc6a7e49b870907aaf53412197c68cff658b4c
Ziheng Wang <zihengw@stanford.edu>
2023-06-09 16:56:03 -0700

    updated

--- my-website/src/pages/index.tsx
+++ my-website/src/pages/index.tsx
@@ -11,15 +11,24 @@ import styles from './index.module.css';
 function HomepageHeader() {
     const {siteConfig} = useDocusaurusContext();
     return (
-      <div className="row">
-        <h1 className="pt-0 subpixel-antialiased mx-auto font-display sm:text-6xl text-4xl font-[Inter,sans-serif] 
-        font-semibold tracking-tight text-slate-900" style={{ lineHeight: '1.1' }}>
-          The Open Source<br></br>
-          <span className="" style={{ color: 'var(--ifm-color-primary-dark)' }}>
-            Security Lake Platform
-          </span> for AWS
+        <div className="row" style={{ backgroundColor: '#EAF6FF', padding: '40px', borderRadius: '10px' }}>
+        <h1 className="pt-0 subpixel-antialiased mx-auto font-display sm:text-7xl text-5xl font-[Inter,sans-serif] 
+            font-semibold tracking-tight text-slate-900" style={{ lineHeight: '1.2', marginLeft: '20px' }}>
+            The Professional<br />
+            <span className="" style={{ color: 'var(--ifm-color-primary-dark)', fontSize: '28px' }}>
+            Crypto/Stocks/Options/Futures
+            </span>  Trading Engine for Every Pepe
         </h1>
-      </div>
+        <div className="signup_container" style={{ marginTop: '40px' }}>
+            <div className="row">
+            <h1 className="title" style={{ fontSize: '28px', marginBottom: '20px' }}>Join the Waitlist</h1>
+            <div className="form_container">
+                <SignupForm />
+            </div>
+            </div>
+        </div>
+        </div>
+
     );
 }
 
@@ -30,15 +39,6 @@ export default function Home(): JSX.Element {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
-      <div className="signup_container">
-        <div className="row">
-            <h1 className="title">Join the Waitlist. </h1>
-            <div className="form_container">
-            <SignupForm />
-      </div>
-      </div>
-      
-      </div>
       <main>
         <HomepageFeatures />
       </main>
