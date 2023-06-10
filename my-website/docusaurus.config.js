3a52fb356d42995e0248431157af70b40ca8f2b1
Ziheng Wang <zihengw@stanford.edu>
2023-06-09 23:14:54 -0700

    updated

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -6,9 +6,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Rottnest Trading',
   tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/rottnest.ico',
 
   // Set the production url of your site here
   url: 'https://your-docusaurus-test-site.com',
@@ -60,24 +60,19 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Rottnest',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/rottnest.png',
         },
         items: [
           {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Why we built it',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
         ],
       },
       footer: {
--- my-website/src/pages/index.tsx
+++ my-website/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
@@ -8,35 +8,78 @@ import SignupForm from '../components/HomepageFeatures/signup';
 
 import styles from './index.module.css';
 
-function HomepageHeader() {
-    const {siteConfig} = useDocusaurusContext();
+const ScrollThroughValues: React.FC<{ values: string[] }> = ({ values }) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+  
+    useEffect(() => {
+      const timer = setInterval(() => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % values.length);
+      }, 2000);
+  
+      return () => {
+        clearInterval(timer);
+      };
+    }, [values]);
+  
     return (
-        <div className="row" style={{ backgroundColor: '#EAF6FF', padding: '40px', borderRadius: '10px' }}>
-        <h1 className="pt-0 subpixel-antialiased mx-auto font-display sm:text-7xl text-5xl font-[Inter,sans-serif] 
-            font-semibold tracking-tight text-slate-900" style={{ lineHeight: '1.2', marginLeft: '20px' }}>
-            The Professional<br />
-            <span className="" style={{ color: 'var(--ifm-color-primary-dark)', fontSize: '28px' }}>
-            Crypto/Stocks/Options/Futures
-            </span>  Trading Engine for Every Pepe
-        </h1>
-        <div className="signup_container" style={{ marginTop: '40px' }}>
-            <div className="row">
-            <h1 className="title" style={{ fontSize: '28px', marginBottom: '20px' }}>Join the Waitlist</h1>
-            <div className="form_container">
-                <SignupForm />
-            </div>
-            </div>
-        </div>
-        </div>
-
+      <span className="" style={{ color: 'var(--ifm-color-primary-dark)', fontSize: '48px' }}>
+      {values[currentIndex]}
+      </span>  
     );
+  };
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <div
+      className="row"
+      style={{
+        backgroundColor: '#EAF6FF',
+        padding: '40px',
+        borderRadius: '10px',
+        display: 'flex',
+      }}
+    >
+      
+      <div className = "column">
+      <h1
+        className="pt-0 subpixel-antialiased mx-auto font-display sm:text-12xl text-9xl font-[Inter,sans-serif] font-semibold tracking-tight text-slate-900"
+        style={{ lineHeight: '1.5',  marginLeft: '20px', flex: '1', fontSize: '48px' }}
+      >
+        The Professional<br />
+        <ScrollThroughValues
+          values={['Futures', 'Stocks', 'Crypto', 'Options']}
+        />{' '}
+        Trading Platform<br />
+        for Every Pepe
+      </h1>
+      <div className="signup_container" style={{ lineHeight: '1.2',  marginLeft: '20px', flex: '1', fontSize: '40px' }}>
+        <div className="row">
+          <div style={{ fontSize: '28px', marginBottom: '20px', marginTop: '20px' }}>
+            Join the Waitlist<br />
+            <SignupForm />
+          </div>
+        </div>
+      </div>
+      </div>
+
+      <div style={{ textAlign: 'right', marginLeft: 'auto', marginRight: '0' }}>
+        <img
+          src={require('@site/static/img/mockup.png').default}
+          width="1000"
+          height="670"
+          alt="JPEG Image"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
