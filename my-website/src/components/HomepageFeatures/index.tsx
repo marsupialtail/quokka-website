40390b836e6a11bff9ce8ac138a5c1c97476c67f
Ziheng Wang <zihengw@stanford.edu>
2023-06-09 23:22:53 -0700

    latest

--- my-website/src/components/HomepageFeatures/index.tsx
+++ my-website/src/components/HomepageFeatures/index.tsx
@@ -32,25 +32,25 @@ const FeatureList: FeatureItem[] = [
         </div>
       ), 
     description: (
-        <ul>
-        <li> Deploy profitable strategies in real time. </li>
+      <ul>
+        <li> Deploy profitable strategies with one click. </li>
+        <li> Track strategy performance in real time. </li>
         <li> Only $10/month for each strategy. </li>
-        <li> Second-level resolution for free. </li>
       </ul>
     ),
   },
   {
-    title: 'Powered by Open Source',
+    title: 'Built on Open Source PhD Research',
     Svg: (
         <div>
-          <img src="/path/to/your/image.jpg" alt="JPEG Image" />
+          <img src={require('@site/static/img/pepe_phd.jpeg').default} width="300" height="300"  alt="JPEG Image" />
         </div>
       ), 
     description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
+     <ul>
+        <li> Built with love by PhD students. </li>
+        <li> Built on TB-scale time series analysis tools deployed at real companies. </li>
+      </ul>
     ),
   },
 ];
