ff785a1c60ff1b935d8027c5121f7c6cbc341071
abigailxchen <abigail.chen@berkeley.edu>
2023-07-13 15:20:53 -0400

    fixed font and chart image

--- my-website/.docusaurus/client-modules.js
+++ my-website/.docusaurus/client-modules.js
@@ -1,6 +1,6 @@
 export default [
-  require('/Users/abigailchen/Desktop/rottnest/rottnest/my-website/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/abigailchen/Desktop/rottnest/rottnest/my-website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/abigailchen/Desktop/rottnest/rottnest/my-website/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/abigailchen/Desktop/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/abigailchen/Desktop/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/abigailchen/Desktop/node_modules/@docusaurus/theme-classic/lib/nprogress'),
   require('/Users/abigailchen/Desktop/rottnest/rottnest/my-website/src/css/custom.css'),
 ];
